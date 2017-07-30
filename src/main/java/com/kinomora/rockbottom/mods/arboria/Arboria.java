5f521aa26231a24d89a7ccbf84bfd14314ef9b96
Kinomora <Kinomora@users.noreply.github.com>
2017-07-30 12:00:47 -0400

    Changed build.gradle

--- src/main/java/com/kinomora/rockbottom/mods/arboria/Arboria.java
+++ src/main/java/com/kinomora/rockbottom/mods/arboria/Arboria.java
@@ -10,7 +10,7 @@ import org.newdawn.slick.util.Log;
 
 public class Arboria implements IMod {
 
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "@VERSION@";
     public static final String MOD_NAME = "Arboria";
 
     public static Arboria instance;
