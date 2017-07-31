ec1660f53cbd1d3691167351f693e7b98ee071c5
Kinomora <Kinomora@users.noreply.github.com>
2017-07-30 21:28:14 -0400

    bleh

--- src/main/java/com/kinomora/rockbottom/mods/arboria/tiles/TileMagicWood.java
+++ src/main/java/com/kinomora/rockbottom/mods/arboria/tiles/TileMagicWood.java
@@ -1,14 +1,18 @@
 package com.kinomora.rockbottom.mods.arboria.tiles;
 
 import com.kinomora.rockbottom.mods.arboria.renderer.TileMagicWoodRenderer;
+import com.kinomora.rockbottom.mods.arboria.tileentity.TileEntityNode;
+import de.ellpeck.rockbottom.api.entity.player.AbstractEntityPlayer;
 import de.ellpeck.rockbottom.api.render.tile.ITileRenderer;
 import de.ellpeck.rockbottom.api.tile.TileBasic;
+import de.ellpeck.rockbottom.api.tile.entity.TileEntity;
 import de.ellpeck.rockbottom.api.tile.state.BoolProp;
 import de.ellpeck.rockbottom.api.util.BoundBox;
 import de.ellpeck.rockbottom.api.util.Util;
 import de.ellpeck.rockbottom.api.util.reg.IResourceName;
 import de.ellpeck.rockbottom.api.world.IWorld;
 import de.ellpeck.rockbottom.api.world.TileLayer;
+import org.newdawn.slick.util.Log;
 
 public class TileMagicWood extends TileBasic {
 
@@ -31,6 +35,23 @@ public class TileMagicWood extends TileBasic {
     @Override
     public BoundBox getBoundBox(IWorld world, int x, int y) {
         return world.getState(x, y).get(IS_NATURAL) ? null : DEFAULT_BOUNDS;
+    }
+
+    @Override
+    public boolean canProvideTileEntity() {
+        return true;
+    }
+
+    @Override
+    public TileEntity provideTileEntity(IWorld world, int x, int y) {
+        return new TileEntityNode(world, x, y);
+    }
+
+    @Override
+    public boolean onInteractWith(IWorld world, int x, int y, TileLayer layer, double mouseX, double mouseY, AbstractEntityPlayer player) {
+        TileEntityNode node = world.getTileEntity(x,y,TileEntityNode.class);
+        Log.debug("Current sipa level: " + node.currentSipa);
+        return true;
     }
 
     @Override
